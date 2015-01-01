3feceda2be5bf58fa6478186e270e3ea9a5816c5
Billiam <billiamthesecond@gmail.com>
2015-01-01 17:58:38 -0600

    Add css minification, boilerplate cleanup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,6 @@
 // Generated on 2014-11-16 using generator-chrome-extension 0.2.11
 'use strict';
 
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
 
   // Load grunt tasks automatically
@@ -110,6 +104,17 @@ module.exports = function (grunt) {
       }
     },
 
+    cssmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/styles',
+          src: ['options.css'],
+          dest: '<%= config.dist %>/styles'
+        }]
+      }
+    },
+    
     // Make sure code styles are up to par and there are no obvious mistakes
     jshint: {
       options: {
@@ -119,17 +124,8 @@ module.exports = function (grunt) {
       all: [
         'Gruntfile.js',
         '<%= config.app %>/scripts/{,*/}*.js',
-        '!<%= config.app %>/scripts/vendor/*',
-        'test/spec/{,*/}*.js'
+        '!<%= config.app %>/scripts/vendor/*'
       ]
-    },
-    mocha: {
-      all: {
-        options: {
-          run: true,
-          urls: ['http://localhost:<%= connect.options.port %>/index.html']
-        }
-      }
     },
 
     // The following *-min tasks produce minifies files in the dist folder
@@ -144,27 +140,6 @@ module.exports = function (grunt) {
       }
     },
 
-    htmlmin: {
-      dist: {
-        options: {
-          // removeCommentsFromCDATA: true,
-          // collapseWhitespace: true,
-          // collapseBooleanAttributes: true,
-          // removeAttributeQuotes: true,
-          // removeRedundantAttributes: true,
-          // useShortDoctype: true,
-          // removeEmptyAttributes: true,
-          // removeOptionalTags: true
-        },
-        files: [{
-          expand: true,
-          cwd: '<%= config.app %>',
-          src: '*.html',
-          dest: '<%= config.dist %>'
-        }]
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       debug: {
@@ -174,7 +149,8 @@ module.exports = function (grunt) {
           cwd: '<%= config.app %>',
           dest: '<%= config.dist %>',
           src: [
-            'images/{,*/}*.png'
+            'images/{,*/}*.png',
+            'styles/{,*/}*.css',
           ]
         }]
       },
@@ -189,7 +165,6 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.{webp,gif}',
             '*.html',
-            'styles/{,*/}*.css',
             'styles/fonts/{,*/}*.*',
             '_locales/{,*/}*.json'
           ]
@@ -233,11 +208,6 @@ module.exports = function (grunt) {
     grunt.config.merge({config: {uglify: true}});
   });
   
-  grunt.registerTask('test', [
-    'connect:test',
-    'mocha'
-  ]);
-  
   grunt.registerTask('debug', function () {
     grunt.task.run([
       'jshint',
@@ -252,18 +222,14 @@ module.exports = function (grunt) {
     'clean:dist',
     'config:prod',
     'imagemin',
-    'htmlmin',
-    // No UI feature selected, cssmin task will be commented
-    // 'cssmin',
+    'cssmin',
     'copy:dist',
     'browserify',
-    'usemin',
     'compress'
   ]);
 
   grunt.registerTask('default', [
     'jshint',
-    'test',
     'build'
   ]);
 };
